c27f2ad6cf1a6405583c7e5f9a62ddbdeb49fe15
sofochka87 <pizzasonya@gmail.com>
2018-06-16 18:07:18 -0400

    print test end time stamp

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import javax.management.RuntimeErrorException;
@@ -57,6 +58,7 @@ public class DiceJobSearch {
         
         driver.close();
         
+        System.out.println("TEST COMPLITED - "+ LocalDateTime.now());
         
         
         
